231ec8c758517bb9f3cba0bda8f36df42b246fa3
hotpocket <githotpocket@gmail.com>
2024-04-05 12:21:00 -0700

    more grunt work.  still not working

--- Gruntfile.cjs
+++ Gruntfile.cjs
@@ -1,97 +1,91 @@
-module.exports = function(grunt) {
-  require('load-grunt-tasks')(grunt);
+module.exports = function (grunt) {
+  require("load-grunt-tasks")(grunt);
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-
-
+    pkg: grunt.file.readJSON("package.json"),
+    ts: {
+      default: {
+        tsconfig: true,
+      },
+      options: {
+        fast: "never",
+      },
+    },
     babel: {
       options: {
         sourceMap: true,
-        presets: ['@babel/preset-env']
+        presets: ["@babel/preset-env"],
       },
       dist: {
-        files: [{
-          expand: true,
-          cwd: 'src',
-          src: ['**/*.js'],
-          dest: 'build',
-          ext: '.js'
-        },{
-          expand: true,
-          cwd: 'src',
-          src: ['**/*.tsx'],
-          dest: 'build',
-          ext: '.tsx'
-        }]
-      }
+        files: [
+          {
+            expand: true,
+            cwd: "src",
+            src: ["**/*.js"],
+            dest: "build",
+            ext: ".js",
+          },          {
+            expand: true,
+            cwd: "src",
+            src: ["**/*.ts"],
+            dest: "build",
+            ext: ".ts",
+          },
+        ],
+      },
     },
 
-
-  concat: {
+    concat: {
       js: {
-        src: ['build/**/*.js'],
-        dest: 'dist/<%= pkg.name %>.js',
+        src: ["build/**/*.js"],
+        dest: "dist/<%= pkg.name %>.js",
       },
       css: {
-        src: ['src/**/*.css'],
-        dest: 'dist/<%= pkg.name %>.css',
+        src: ["src/**/*.css"],
+        dest: "dist/<%= pkg.name %>.css",
       },
     },
-
 
     terser: {
       dist: {
         files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.js.dest %>']
-        }
-      }
+          "dist/<%= pkg.name %>.min.js": ["<%= concat.js.dest %>"],
+        },
+      },
     },
-
 
     cssmin: {
       target: {
-        files: [{
-          expand: true,
-          cwd: 'dist',
-          src: ['*.css', '!*.min.css'],
-          dest: 'dist',
-          ext: '.min.css'
-        }]
-      }
+        files: [
+          {
+            expand: true,
+            cwd: "dist",
+            src: ["*.css", "!*.min.css"],
+            dest: "dist",
+            ext: ".min.css",
+          },
+        ],
+      },
     },
-
 
     watch: {
       js: {
-        files: ['src/**/*.js'],
-        tasks: ['babel', 'concat:js', 'terser'],
+        files: ["src/**/*.js"],
+        tasks: ["babel", "concat:js", "terser"],
       },
       css: {
-        files: ['src/**/*.css'],
-        tasks: ['concat:css', 'cssmin'],
+        files: ["src/**/*.css"],
+        tasks: ["concat:css", "cssmin"],
       },
     },
   });
 
+  grunt.loadNpmTasks("grunt-babel");
+  grunt.loadNpmTasks("grunt-ts");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-terser");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-watch");
 
-  grunt.loadNpmTasks('grunt-babel');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-terser');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-
-  grunt.registerTask('default', ['babel', 'concat', 'terser', 'cssmin']);
+  grunt.registerTask("default", ["babel", "concat", "terser", "cssmin"]);
 };
-
-
-
-
-
-
-
-
-
-
-
